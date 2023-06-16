a21e062eb93b518b00096c4c3d22561c2c5cc133
Kalotdh <kalotdh@naver.com>
2023-06-17 01:23:44 +0900

    co2 센서 테스트 코드 업로드, DS ignore

---
+++ arduino/co2.ino
@@ -0,0 +1,24 @@
+#include <MHZ19PWM.h>
+
+const int MHZ19_Pin = 2;
+MHZ19PWM mhz(MHZ19_Pin, MHZ_CONTINUOUS_MODE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Starting..."));
+
+  delay(2000);
+
+  mhz.useLimit(5000);
+}
+
+void loop() {
+zddddz
+  delay(1000);
+}
+
+float readCO2() {
+  float co2 = mhz.getCO2();
+  Serial.println(co2);
+  return co2;
+}
